460204d1055d13a913a7cb4970de283ca32ca302
nik frank <nikbfrank@gmail.com>
2018-10-22 17:04:32 -0700

    basic quote machine

--- seinfeld/src/App.js
+++ seinfeld/src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { Dropdown } from 'component-lib-boilerplate';
 
+import jerry from './jerry.png';
+import kramer from './kramer.png';
+
+const people = {
+  jerry, kramer
+};
+
 class App extends Component {
+  state = {
+    person: 'jerry',
+    quote: 'yada yada yada',
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Dropdown/>
+
+          
+          <div className='quotation'>
+            <img className='talking-head' src={people[this.state.person]}/>
+            <div className='speech-bubble'>
+              <p>{this.state.quote}</p>
+            </div>
+          </div>
+          <br/>
+          
+          <select value={this.state.person} onChange={e=> this.setState({ person: e.target.value })}>
+            {
+              Object.keys(people).map(name => (
+                <option value={name} key={name}>{name}</option>
+              ) )
+            }
+          </select>
+          <br/>
+          
+          <textarea value={this.state.quote} onChange={e=> this.setState({ quote: e.target.value })}/>
         </header>
       </div>
     );
